2284a86be63f873ab2e10850bfb9eb97bf3e9b79
Izzanauzi <izzanauzi17@gmail.com>
2025-06-21 09:31:52 +0700

    penambahan composer

--- views/partials/head.php
+++ views/partials/head.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Framework</title>
+  <title>Gudang</title>
   <link rel="website icon" href="/assets/img/cube.ico">
   <!-- CSS -->
   <link rel="stylesheet" href="/css/bootstrap.min.css">
